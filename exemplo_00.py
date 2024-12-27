a15a7f49a4938e2b1fcebb439e72591880ff1151
Alexandre <26aandrade26@gmail.com>
2024-12-27 16:54:53 +0000

    Funcao soma incompleta

---
+++ exemplo_00.py
@@ -0,0 +1,10 @@
+def soma(valor_1: float, valor_2: float) -> float:
+    """
+    Função de Soma
+    """
+    valor_3 = valor_1 + valor_2
+    return valor_3
+
+print(f"O resultado é: {soma(2.5,3.7)}")
+
+
